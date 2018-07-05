8d672b0efb18b7e0c2408c69095cb6ec11b8e58f
risunurya <mr.purq@gmail.com>
2018-07-05 23:11:35 +0545

    First Commit

---
+++ src/main/java/com/cms/main/CmsOpensourcesApplication.java
@@ -0,0 +1,12 @@
+package com.cms.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsOpensourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsOpensourcesApplication.class, args);
+	}
+}
